ff761679a5cf2c78d575dc13e6860e0eac436d39
iparinile <poselennov.il@ya.ru>
2020-12-22 19:05:12 +0500

    Using Templates in Our Headlines Project

--- Headlines Project/headlines.py
+++ Headlines Project/headlines.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 app = Flask(__name__)
@@ -11,15 +12,7 @@ RSS_FEED = {'mail': 'https://news.mail.ru/rss/main/74/',
 @app.route("/<publication>")
 def get_news(publication="mail"):
     feed = feedparser.parse(RSS_FEED[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1> Заголовки </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html> """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html", articles=feed['entries'])
 
 
 if __name__ == '__main__':
