18a09587d8c7eacdb91b34e41eb45fdb95a1e17d
iparinile <poselennov.il@ya.ru>
2020-12-23 11:10:43 +0500

    currency

--- Headlines Project/headlines.py
+++ Headlines Project/headlines.py
@@ -13,7 +13,18 @@ RSS_FEEDS = {'mail': 'https://news.mail.ru/rss/main/74/',
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=3672b074c7480ad9255662ebe5441a3f'
 
-DEFAULTS = {'publication': 'mail', 'city': 'London,UK'}
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=76f0e8ff9d27491293c9a5ffbf339c28"
+
+
+def get_rate(frm, to):
+    all_currency = urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate / frm_rate, parsed.keys()
+
+
+DEFAULTS = {'publication': 'mail', 'city': 'London,UK', 'currency_from': 'GBP', 'currency_to': 'USD'}
 
 
 @app.route("/")
@@ -28,7 +39,21 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html",
+                           articles=articles,
+                           weather=weather,
+                           currency_from=currency_from,
+                           currency_to=currency_to,
+                           rate=rate,
+                           currencies=sorted(currencies))
 
 
 def get_news(query):
@@ -49,7 +74,8 @@ def get_weather(query):
     if parsed.get('weather'):
         weather = {'description': parsed['weather'][0]['description'],
                    'temperature': parsed['main']['temp'],
-                   'city': parsed['name']
+                   'city': parsed['name'],
+                   'country': parsed['sys']['country']
                    }
     return weather
 
